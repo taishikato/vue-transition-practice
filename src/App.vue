b2fa36a72b424c072b6461b9f5a03600c67cae7b
taishikato <taishi.k0903@gmail.com>
2017-12-25 23:31:28 +0900

    done

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Modal/>
+    <TransitionModal/>
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal'
+import TransitionModal from './components/TransitionModal'
+
+export default {
+  name: 'app',
+  components: {
+    Modal,
+    TransitionModal
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <button v-on:click="show = !show">
+    Toggle
+  </button>
+  <transition name="fade">
+    <p v-if="show">hello</p>
+  </transition>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  data() {
+    return {
+      show: true
+    }
+  }
+}
+</script>
+
+<style>
+</style>
